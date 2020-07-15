0030a0efe2ef8d74c587cc1100981029a23ee6a0
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-07-15 17:58:50 -0300

    Alinhando verticalmente e Horizontalmente

--- Auto Layout/ViewController.swift
+++ Auto Layout/ViewController.swift
@@ -14,28 +14,58 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let redView = UIView()
-        
-        view.addSubview(redView)
-        
         redView.backgroundColor = .red
         
-        // redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+//
+//        view.addSubview(redView)
+//        view.addSubview(blueView)
+//
+//
+//        redView.translatesAutoresizingMaskIntoConstraints = false
+//        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//
+//        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+//
+//        blueView.translatesAutoresizingMaskIntoConstraints = false
+//        blueView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+//        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//
+//        blueView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
         
-        // setar as constraints de forma manual
-        redView.translatesAutoresizingMaskIntoConstraints = false
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
         
-//        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 50).isActive = true
-//        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
-        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
-        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
+////        // redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+////
+////        // setar as constraints de forma manual
+////        redView.translatesAutoresizingMaskIntoConstraints = false
+////
+//////        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+//////        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 50).isActive = true
+//////        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+//////        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+//
+//        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+//
+//        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//
+//        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//
         
         
     }
